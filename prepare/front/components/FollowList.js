04032f03b25e189d9dbcb2c8586d971625d2f65c
Vicky <hwfongfing@gmail.com>
2021-02-14 00:56:19 +0900

    feat: 프로필 페이지 구현

---
+++ prepare/front/components/FollowList.js
@@ -0,0 +1,40 @@
+﻿import React, { useMemo } from "react";
+import PropTypes from "prop-types";
+import { List, Button, Card } from "antd";
+import { StopOutlined } from "@ant-design/icons";
+
+const FollowList = ({ header, data }) => {
+  const listStyle = useMemo(() => ({ marginBottom: 20 }), []);
+  const loadMoreStyle = useMemo(() => ({ textAlign: "center", margin: "10px 0" }), []);
+  const renderListStyle = useMemo(() => ({ marginTop: 20 }), []);
+
+  return (
+    <List
+      header={<div>{header}</div>}
+      style={listStyle}
+      grid={{ gutter: 4, xs: 2, md: 3 }}
+      size="small"
+      loadMore={
+        <div style={loadMoreStyle}>
+          <Button>더보기</Button>
+        </div>
+      }
+      bordered
+      dataSource={data}
+      renderItem={(item) => (
+        <List.Item style={renderListStyle}>
+          <Card actions={[<StopOutlined key="stop" />]}>
+            <Card.Meta description={item.nickname} />
+          </Card>
+        </List.Item>
+      )}
+    />
+  );
+};
+
+FollowList.propTypes = {
+  header: PropTypes.string.isRequired,
+  data: PropTypes.array.isRequired,
+};
+
+export default FollowList;
--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,14 +1,22 @@
 ﻿import AppLayout from "../components/AppLayout";
 import Head from "next/head";
 
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
+
 const Profile = () => {
+  const followerList = [{ nickname: "비키" }, { nickname: "워니" }, { nickname: "썬" }];
+  const followingList = [{ nickname: "비키" }, { nickname: "워니" }, { nickname: "썬" }];
+
   return (
     <>
       <Head>
         <title>내 프로필 | NodeBird</title>
       </Head>
       <AppLayout>
-        <div>내 프로필</div>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
       </AppLayout>
     </>
   );
