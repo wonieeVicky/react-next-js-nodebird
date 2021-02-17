cf9e7e50c096c133551845b61a779a657daa3144
Vicky <hwfongfing@gmail.com>
2021-02-17 23:06:26 +0900

    feat: 게시글 해시태그 링크로 구현

---
+++ prepare/front/components/PostCardContent.js
@@ -0,0 +1,26 @@
+﻿import PropTypes from "prop-types";
+import Link from "next/link";
+
+const PostCardContent = ({ postData }) => {
+  return (
+    <>
+      {postData.split(/(#[^\s#]+)/g).map((v, i) => {
+        if (v.match(/(#[^\s#]+)/)) {
+          return (
+            <Link href={`/hashtag/${v.slice(1)}`} key={i}>
+              <a>{v}</a>
+            </Link>
+          );
+        }
+
+        return v;
+      })}
+    </>
+  );
+};
+
+PostCardContent.propTypes = {
+  postData: PropTypes.string.isRequired,
+};
+
+export default PostCardContent;
