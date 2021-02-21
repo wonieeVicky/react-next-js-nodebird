3e5e986cb5279a08de4afdef3c3b575e291daed5
Vicky <hwfongfing@gmail.com>
2021-02-22 00:49:59 +0900

    fix: profile 컴포넌트 접근이 로그인 후에만 가능하도록 에러 개선

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,12 +1,23 @@
 ﻿import AppLayout from "../components/AppLayout";
 import Head from "next/head";
+import Router from "next/router";
 
 import NicknameEditForm from "../components/NicknameEditForm";
 import FollowList from "../components/FollowList";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const Profile = () => {
   const { me } = useSelector((state) => state.user);
+  useEffect(() => {
+    if (!(me && me.id)) {
+      Router.push("/");
+    }
+  }, [me && me.id]);
+
+  if (!me) {
+    return null;
+  }
 
   return (
     <>
