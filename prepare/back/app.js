7d033fa4f05b65ccb3cb18f6391c3121cb1b19ce
Vicky <hwfongfing@gmail.com>
2021-02-23 01:04:45 +0900

    feat: 백엔드 서버 프로젝트 init

---
+++ prepare/back/app.js
@@ -0,0 +1,16 @@
+﻿const http = require("http"); // http가 서버의 역할을 해준다.
+const { isRegExp } = require("util");
+
+// req: request, front에서 넘어온 요청에 대한 정보가 담긴다.
+// res: response, 응답에 대한 정보가 담긴다.
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+  res.write("<h1>hello vicky1</h1>");
+  res.write("<h2>hello vicky2</h2>");
+  res.write("<h3>hello vicky3</h3>");
+  res.write("<h4>hello vicky4</h4>");
+  res.end("<h5>Hello node!!</h5>");
+});
+server.listen(3065, () => {
+  console.log("서버 실행 중");
+});
