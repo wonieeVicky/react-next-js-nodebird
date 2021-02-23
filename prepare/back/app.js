a99a60c1450e468ae0778a0d5786de0096df488f
Vicky <hwfongfing@gmail.com>
2021-02-23 22:11:09 +0900

    feat: 익스프레스로 라우팅 및 라우터 분리

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,16 +1,25 @@
-﻿const http = require("http"); // http가 서버의 역할을 해준다.
-const { isRegExp } = require("util");
+﻿const express = require("express");
+const postRouter = require("./routes/post");
+const app = express();
 
-// req: request, front에서 넘어온 요청에 대한 정보가 담긴다.
-// res: response, 응답에 대한 정보가 담긴다.
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-  res.write("<h1>hello vicky1</h1>");
-  res.write("<h2>hello vicky2</h2>");
-  res.write("<h3>hello vicky3</h3>");
-  res.write("<h4>hello vicky4</h4>");
-  res.end("<h5>Hello node!!</h5>");
+// url /에 보내는 get 메서드
+app.get("/", (req, res) => {
+  res.send("hello express"); // 문자열 응답
 });
-server.listen(3065, () => {
+app.get("/api", (req, res) => {
+  res.send("hello api"); // 문자열 응답
+});
+
+app.get("/api/posts", (req, res) => {
+  res.json([
+    { id: 1, content: "hello1" },
+    { id: 2, content: "hello2" },
+    { id: 3, content: "hello3" },
+  ]); // json 객체 응답
+});
+
+app.use("/post", postRouter); // postRouter api path에 /post가 접두어(prefix)로 붙는다.
+
+app.listen(3065, () => {
   console.log("서버 실행 중");
 });
---
+++ prepare/back/routes/post.js
@@ -0,0 +1,17 @@
+﻿// 노드에서는 import, export를 쓰지않고 require와 module.exports를 사용한다.
+// 프론트에서도 import, export를 webpack이 require와 module.exports로 변환해주는 개념임
+// 향후에는 import, export로 합쳐질 것으로 예상한다.
+const express = require("express");
+
+const router = express.Router();
+
+// POST /post
+router.post("/", (req, res) => {
+  res.json({ id: 1, content: "hello1" });
+});
+// DELETE /post
+router.delete("/", (req, res) => {
+  res.json({ id: 1 });
+});
+
+module.exports = router;
