23df68cdb28002bfbc5c8920ce380a9745a4ee4a
Vicky <hwfongfing@gmail.com>
2021-03-24 23:46:27 +0900

    fix: lambda

--- prepare/lambda/index.js
+++ prepare/lambda/index.js
@@ -9,7 +9,7 @@ const s3 = new AWS.S3();
 exports.handler = async (event, context, callback) => {
   const Bucket = event.Records[0].s3.bucket.name; // vicky-nodebird-s3
   const Key = decodeURIComponent(event.Records[0].s3.object.key); // original/123123_abc.png
-  console.log(Bucket, key);
+  console.log(Bucket, Key);
 
   const filename = Key.split('/')[Key.split('/').length - 1];
   const ext = Key.split('.')[Key.split('.').length - 1].toLowerCase();
